f7ae0d962161a0ba706b459aec255121ed260054
Drakar4995 <the-inic@hotmail.com>
2022-09-27 22:12:29 +0200

    añadido header y footer compartido

---
+++ public/javascripts/header.js
@@ -0,0 +1,8 @@
+//Header
+const header = document.createElement('header')
+header.innerHTML = '<image src="../images/logo.png"> </image>'
+header.innerHTML += '<h1 class="title">Sitio Web de Tareas</h1>'
+
+document.body.insertAdjacentElement("afterbegin",header)
+
+
