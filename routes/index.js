78af7fbc84748c28f7bebd2000a211160d979c7d
Drakar4995 <the-inic@hotmail.com>
2022-09-27 15:27:06 +0200

    Base

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', (req, res)=> {
+  console.log('dentro');
+  res.redirect('./tareasApp/index.html');
+});
+
+
+
+
+module.exports = router;
