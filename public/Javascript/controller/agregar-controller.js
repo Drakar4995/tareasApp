4f71aa9603a371052bd99cd6ce51660ffeadfa59
Bafnir <98287303+Bafnir@users.noreply.github.com>
2022-11-08 22:00:33 +0100

    Revert "Merge pull request #9 from Drakar4995/nikolai"
    This reverts commit 6417a76921ae2e821157d12553f60957d626807f, reversing
    changes made to f86b33743e6794d592658497ee8963e581630d41.

--- public/Javascript/controller/agregar-controller.js
+++ public/Javascript/controller/agregar-controller.js
@@ -1,7 +1,5 @@
 class AgregarController extends PageController{
     constructor(regex,mode,view){
         super(regex,mode,view);
-        this.agregarTareaController = new AgregarTareaController(model,view.agregarTareaView);
     }
-
 }
