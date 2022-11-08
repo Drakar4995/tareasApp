9553e5000f7e54485fac40e3a75df6a93aea3820
Drakar4995 <the-inic@hotmail.com>
2022-11-08 18:10:44 +0100

    Agregada funcion:AgregarTarea

--- public/Javascript/views/agregar-page-view.js
+++ public/Javascript/views/agregar-page-view.js
@@ -5,27 +5,27 @@ class AgregarPageView extends PageView{
         Agregar Tarea
        </p>
        
-       <div class="div-delete-task">git
-        <a class="top-spacer" href="/tareasApp/listado" onclick="router.route('')">
+       <div class="div-delete-task">
+        <a class="top-spacer" href="/tareasApp/listado" onclick="router.route()">
             Listado
         </a>
        
         <form>
             <label class="titleform" for="titulo">Titulo</label> <br>
-            <input class="text" type="text"> <br>
+            <input class="text" id="titulo" type="text"> <br>
        
             <label class="titleform" for="descripcion">Descripcion</label> <br>
-            <textarea class="textarea"> </textarea>
+            <textarea class="textarea" id="descripcion"> </textarea>
         </form>
-        <a class="logo-link" href="ver-tarea.html">
-            <input class="boton-delete" type="button" value="Guardar" />
+        <button type="button" onclick="agregarController.agregarTareaController.onGuardarClick(event)">Agregar</button>
         </a>
         <button class=boton-delete type="button"> Limpiar </button>
        </div>`;
+       this.agregarTareaView = new AgregarTareaView(model,"agregarTarea");
     }
-
     refresh(){
         super.refresh();
+        this.agregarTareaView.refresh();
     }
 
 }
