a9dd7c78edc60fe0d02101fb6de4a688faf21a5f
Bafnir <98287303+Bafnir@users.noreply.github.com>
2022-11-08 22:00:45 +0100

    Revert "Agregada funcion:AgregarTarea"
    This reverts commit 9553e5000f7e54485fac40e3a75df6a93aea3820.

--- public/Javascript/views/agregar-page-view.js
+++ public/Javascript/views/agregar-page-view.js
@@ -5,14 +5,14 @@ class AgregarPageView extends PageView{
         Agregar Tarea
        </p>
        
-       <div class="div-delete-task">
-        <a class="top-spacer" href="/tareasApp/listado" onclick="router.route()">
+       <div class="div-delete-task">git
+        <a class="top-spacer" href="/tareasApp/listado" onclick="router.route('')">
             Listado
         </a>
        
         <form>
             <label class="titleform" for="titulo">Titulo</label> <br>
-            <input class="text" id="titulo" type="text"> <br>
+            <input class="text" type="text"> <br>
        
             <label class="titleform" for="descripcion">Descripcion</label> <br>
             <textarea class="textarea"> </textarea>
