a260a5afbaef176065347fb936e0e5fca6bcc49c
Bafnir <98287303+Bafnir@users.noreply.github.com>
2022-11-08 22:19:19 +0100

    Restore de main

--- public/Javascript/views/agregar-page-view.js
+++ public/Javascript/views/agregar-page-view.js
@@ -5,14 +5,12 @@ class AgregarPageView extends PageView{
         Agregar Tarea
        </p>
        
-       <div class="div-delete-task">git
-        <a class="top-spacer" href="/tareasApp/listado" onclick="router.route('')">
-            Listado
-        </a>
+       <div class="div-delete-task">
+        <a class="top-spacer-form" href="/tareasApp/listado" onclick="router.route('')">Listado</a>
        
         <form>
             <label class="titleform" for="titulo">Titulo</label> <br>
-            <input class="text" type="text"> <br>
+            <input class="text" id="titulo" type="text"> <br>
        
             <label class="titleform" for="descripcion">Descripcion</label> <br>
             <textarea class="textarea"> </textarea>
