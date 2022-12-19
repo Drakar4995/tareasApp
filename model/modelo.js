b167e0485f02937f23e44bcbe41ba17a898fd92f
Drakar4995 <the-inic@hotmail.com>
2022-12-19 09:09:51 +0100

    Añadida cerrar la conexion a la bbdd cuando se cierra el servidor

--- model/modelo.js
+++ model/modelo.js
@@ -6,22 +6,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 class TareaApp {
   constructor() {
-    var db = mongoose.connection;
-    db.on("connecting", function () {
-      console.log("Connecting to ", uri);
-    });
-    db.on("connected", function () {
-      console.log("Connected to ", uri);
-    });
-    db.on("disconnecting", function () {
-      console.log("Disconnecting from ", uri);
-    });
-    db.on("disconnected", function () {
-      console.log("Disconnected from ", uri);
-    });
-    db.on("error", function (err) {
-      console.error("Error ", err.message);
-    });
+    
   }
 
   async borrarTodo() {
